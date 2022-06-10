fd1e1d0458626206157f466c0be6a329918e610f
rathes007 <mailramratheesh47@gmail.com>
2022-06-11 00:58:32 +0530

    testing hello file

---
+++ hello.py
@@ -0,0 +1,26 @@
+def fonts(doc, granularity=False):
+    styles = {}
+    font_counts = {}
+
+    for page in doc:
+        blocks = page.getText("dict")["blocks"]
+        for b in blocks:  # iterate through the text blocks
+            if b['type'] == 0:  # block contains text
+                for l in b["lines"]:  # iterate through the text lines
+                    for s in l["spans"]:  # iterate through the text spans
+                        if granularity:
+                            identifier = "{0}_{1}_{2}_{3}".format(s['size'], s['flags'], s['font'], s['color'])
+                            styles[identifier] = {'size': s['size'], 'flags': s['flags'], 'font': s['font'],
+                                                  'color': s['color']}
+                        else:
+                            identifier = "{0}".format(s['size'])
+                            styles[identifier] = {'size': s['size'], 'font': s['font']}
+
+                        font_counts[identifier] = font_counts.get(identifier, 0) + 1  # count the fonts usage
+
+    font_counts = sorted(font_counts.items(), key=itemgetter(1), reverse=True)
+
+    if len(font_counts) < 1:
+        raise ValueError("Zero discriminating fonts found!")
+
+    return font_counts, styles
